9a5e1a0204c3f039c74d78748af3c58d085cefca
Thomas Kadauke <tkadauke@imedo.de>
2012-12-02 18:01:06 +0100

    Move loggers over form motion-resource to motion-support

--- lib/motion-resource/logger.rb
+++ lib/motion-resource/logger.rb
@@ -1,17 +1,6 @@
 module MotionResource
-  class NullLogger
-    def log(string)
-    end
-  end
-
-  class StdoutLogger
-    def log(string)
-      puts string
-    end
-  end
-
   class Base
     class_inheritable_accessor :logger
-    self.logger = NullLogger.new
+    self.logger = MotionSupport::NullLogger.new
   end
 end
